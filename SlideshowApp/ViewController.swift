dbc88075e94081573a2265e52ce4efd1175f8f16
Kazuki Miki <milkeyway077@gmail.com>
2021-04-15 23:17:07 +0900

    微修正

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -12,17 +12,20 @@ class ViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     let dispImageNo = ["image1","image2","image3"]
 
+    
+    @IBOutlet weak var susumu: UIButton!
+    @IBOutlet weak var modoru: UIButton!
+    
     var imageArror = 0
     var timer : Timer!
-    var timer_sec: Float = 0
     
     @IBAction func nextstep(_ sender: Any) {
         if timer == nil{
         imageArror += 1
         display()
         imageView.image = UIImage(named: dispImageNo[imageArror])
-        
         }
+        
     }
     @IBAction func goback(_ sender: Any) {
         if timer == nil{
@@ -33,7 +36,6 @@ class ViewController: UIViewController {
     }
     
     @objc func Ontimer(_ timer: Timer) {
-        timer_sec += 2.0
         imageView.image = UIImage(named: dispImageNo[imageArror])
         imageArror += 1
         display()
@@ -41,20 +43,24 @@ class ViewController: UIViewController {
     
     @IBAction func startend(_ sender: Any) {
         if (timer == nil) {
-        
-        timer = Timer.scheduledTimer(timeInterval: 2.0,target: self, selector: #selector(ViewController.Ontimer(_:)),userInfo: nil, repeats: true)
+            
+            timer = Timer.scheduledTimer(timeInterval: 2.0,target: self, selector: #selector(ViewController.Ontimer(_:)),userInfo: nil, repeats: true)
+            susumu.isEnabled = false
+            modoru.isEnabled = false
         } else {
             timer.invalidate()
             timer = nil
+            susumu.isEnabled = true
+            modoru.isEnabled = true
         }
     }
   
     func display(){
         if imageArror > 2{
-        imageArror = 0
+            imageArror = 0
         }
         if imageArror < 0{
-        imageArror = 2
+            imageArror = 2
         }
     }
     
@@ -64,12 +70,19 @@ class ViewController: UIViewController {
         imageView.image = UIImage(named:dispImageNo[imageArror])
     }
     
-    @IBAction func tapView(_ sender: Any) {
+    @IBAction func tapview(_ sender: Any) {
     }
+    
+    
     @IBAction func unwind(_ segue: UIStoryboardSegue) {
       }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let resultViewController:ResultViewController = segue.destination as! ResultViewController
+        
+        if timer != nil{
+        timer.invalidate()
+        }
+        
         resultViewController.x = imageView.image!
     }
        
